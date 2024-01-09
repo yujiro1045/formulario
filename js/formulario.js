d0ef85405d6dfb7c1cd96e4614b5cf8278c82312
Juan Medina <juanmedi730@gmail.com>
2024-01-09 16:56:18 -0500

    segundo commits

--- js/formulario.js
+++ js/formulario.js
@@ -4,9 +4,9 @@ const inputs = document.querySelectorAll("#formulario input");
 const expresiones = {
   usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
   nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-  password: /^.{4,12}$/, // 4 a 12 digitos.
+  password: /^.{4,14}$/, // 4 a 14 digitos.
   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-  telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+  telefono: /^\d{7,10}$/, // 7 a 10 numeros.
 };
 
 const campos = {
